e93e7d2826b4dcb7e4db3db886e964750eca05d5
Ludovic Barman <ludovic.barman@protonmail.com>
2019-10-26 14:44:19 +0200

    Dusk test pass

--- website/data/resources/views/welcome.blade.php
+++ website/data/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Test Lbarman
                 </div>
 
                 <div class="links">
--- website/data/tests/Browser/ExampleTest.php
+++ website/data/tests/Browser/ExampleTest.php
@@ -16,7 +16,7 @@ class ExampleTest extends DuskTestCase
     public function testBasicExample()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')->dump();
+            $browser->visit('http://nginx/')->assertSee('Laravel - Test Lbarman');
         });
     }
 }
